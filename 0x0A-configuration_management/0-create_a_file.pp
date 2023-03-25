aa2bc16ae034d491dff6b4a190b180eae6641d6c
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-25 01:35:07 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create file in /tmp
 file { '/tmp/holberton':
-  owner   => www-data,
-  group   => www-data,
+  owner   => 'www-data',
+  group   => 'www-data',
   mode    => '0744',
   content => 'I love Puppet'
   }
