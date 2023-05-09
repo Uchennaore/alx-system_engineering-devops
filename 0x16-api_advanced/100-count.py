5fafb61d68d6e12ef33505668f00a301d490a755
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-05-10 00:59:25 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,25 +1,55 @@
 #!/usr/bin/python3
-import requests
+"""
+parses the title of all hot articles, and prints a sorted count of given
+keywords (case-insensitive, delimited by spaces)
+"""
+from collections import OrderedDict
+from requests import get
 
 
+def count_words(subreddit, word_list, after=None, match_dict={}):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces)
+    Args:
+        subreddit (str): subreddit
+        word_list (list): list of words to count occurrences for
+        after (str): refer to next page
+        match_dict (dict): dictionary of frequency of words from word_list
+        flag (int): indicate when match_dict is complete
+    """
+    try:
+        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+                'after={}'.format(subreddit, after),
+                headers={'User-Agent': 'bc'})
+        sub_dict = r.json()
 
-def count_words(subreddit, word_list):
-    """script that queries Reddit API then parses the title of all hot articles
-    Must be recursive
-    Results should be printed in descending order, by the count, not the title. Words with no matches should be skipped and not printed.
-    Results are based on the number of times a keyword appears, not titles it appears in. 'java java java' counts as 3 separate occurences of java.
-    To make life easier, 'java.' or 'java!' or 'java_' should not count as 'java'
-    Hints:
-        Ensure that you are NOT following redirects.
-        Your code will NOT pass if you are using a loop and not recursively calling the function!
-        This /can/ be done with a loop but the point is to use a recursive function. :)
-    Returns:
-        sorted count of given keywords
-               case-insensitive
-               delimited by spaces.
-                         exampleJavascript should count as javascript, but java should not).
-        If no posts match or the subreddit is invalid, print a newline.
-   """
+        if match_dict == {}:
+            for w in word_list:
+                match_dict[w] = 0
 
+        after = sub_dict['data']['after']
 
-    
+        for i in range(len(sub_dict['data']['children'])):
+            title_string = sub_dict['data']['children'][i]['data']['title']
+            search_list = title_string.split()
+            for word in search_list:
+                for w in word_list:
+                    if w.lower() == word.lower():
+                        match_dict[w] += 1
+
+        if after is None:
+            descend_dict = OrderedDict(sorted(match_dict.items(),
+                                              key=lambda x: x[1],
+                                              reverse=True))
+            zero_count = 0
+            for k, v in descend_dict.items():
+                if v != 0:
+                    print("{}: {}".format(k, v))
+                else:
+                    zero_count += 1
+            if zero_count == len(descend_dict):
+                print()
+        else:
+            count_words(subreddit, word_list, after, match_dict)
+    except:
+        pass
